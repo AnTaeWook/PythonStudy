528c0a1ebd1bf057791623ca832ebe12cabdfc89
AnTaeWook <antk7894@naver.com>
2021-12-27 16:35:25 +0900

    change

--- Basic/remove_item_all.py
+++ Basic/remove_item_all.py
@@ -1,9 +1,9 @@
 # 리스트 내 특정 원소 모두 제거하기
 # remove()로는 하나밖에 못함
 
-list = [1, 2, 2, 3, 4, 5, 5, 6, 7, 8, 8, 8, 9]
+list_old = [1, 2, 2, 3, 4, 5, 5, 6, 7, 8, 8, 8, 9]
 remove_set = [2, 5, 8]
 
-list_new = [i for i in list if i not in remove_set]
+list_new = [i for i in list_old if i not in remove_set]
 
 print(list_new)
