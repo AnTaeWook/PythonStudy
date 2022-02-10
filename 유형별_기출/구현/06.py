0a6f32cb0902b66ed295b93b8e84829a20b002ba
AnTaeWook <antk7894@naver.com>
2022-02-10 09:30:33 +0900

    220210

--- 유형별_기출/구현/06.py
+++ 유형별_기출/구현/06.py
@@ -1,69 +1,76 @@
+from tkinter import E
+
+
 def solution(n, build_frame):
     
     # 답으로 제출할 건축물들 상태 배열
     status = []
     
     # 빌드 프레임을 순회하며 하나씩 건설 혹은 무시
-    for build in build_frame:
+    for [x, y, build, e] in build_frame:
         flag = 0
         
         # 건축물이 '기둥'일 경우 
-        if build[2] == 0:
+        if build == 0:
             # 설치일 경우
-            if build[3] == 1:
+            if e == 1:
                 # 설치할 수 있는 조건
-                if build[1] == 0 or ([build[0], build[1] - 1, 0] in status) or ([build[0] - 1, build[1], 1] in status) or ([build[0], build[1], 1] in status):
-                    status.append([build[0], build[1], 0])
+                if y == 0 or ([x, y - 1, 0] in status) or ([x - 1, y, 1] in status) or ([x, y, 1] in status):
+                    status.append([x, y, 0])
             # 삭제일 경우
             else:
                 # 기둥 위에 기둥이나 보가 있을 경우
-                if ([build[0], build[1] + 1, 0] in status) or ([build[0], build[1] + 1, 1] in status) or ([build[0] - 1, build[1] + 1, 1] in status):
-                    if ([build[0], build[1] + 1, 1] in status) or ([build[0] - 1, build[1] + 1, 1] in status):
-                        if [build[0], build[1] + 1, 1] in status:
-                            if (([build[0] - 1, build[1] + 1, 1] in status) and ([build[0] + 1, build[1] + 1, 1] in status)) or ([build[0] + 1, build[1], 0] in status):
+                if ([x, y + 1, 0] in status) or ([x, y + 1, 1] in status) or ([x - 1, y + 1, 1] in status):
+                    if ([x, y + 1, 1] in status) or ([x - 1, y + 1, 1] in status):
+                        if [x, y + 1, 1] in status:
+                            if (([x - 1, y + 1, 1] in status) and ([x + 1, y + 1, 1] in status)) or ([x + 1, y, 0] in status):
                                 flag = 1
                             else:
                                 continue
-                        if [build[0] - 1, build[1] + 1, 1] in status:
-                            if (([build[0] - 2, build[1] + 1, 1] in status) and ([build[0], build[1] + 1, 1] in status)) or ([build[0] - 1, build[1], 0] in status):
+                        if [x - 1, y + 1, 1] in status:
+                            if (([x - 2, y + 1, 1] in status) and ([x, y + 1, 1] in status)) or ([x - 1, y, 0] in status):
                                 flag = 1
                             else:
                                 continue
                     if flag:
-                        status.remove([build[0], build[1], 0])
+                        status.remove([x, y, 0])
                 else: 
-                    status.remove([build[0], build[1], 0])
+                    status.remove([x, y, 0])
         
         # 건축물이 '보'일 경우
         else:
             
             # 설치일 경우
-            if build[3] == 1:
+            if e == 1:
                 # 설치할 수 있는 조건
-                if (([build[0] - 1, build[1], 1] in status) and ([build[0] + 1, build[1], 1] in status)) or ([build[0], build[1] - 1, 0] in status) or ([build[0] + 1, build[1] - 1, 0] in status):
-                    status.append([build[0], build[1], 1])
+                if (([x - 1, y, 1] in status) and ([x + 1, y, 1] in status)) or ([x, y - 1, 0] in status) or ([x + 1, y - 1, 0] in status):
+                    status.append([x, y, 1])
             # 삭제일 경우
             else:
-                if ([build[0], build[1] - 1, 0] not in status) and ([build[0] + 1, build[1] - 1, 0] not in status):
-                    if ([build[0] - 1, build[1] - 1, 0] in status) and ([build[0] + 2, build[1] - 1, 0] in status):
-                        status.remove([build[0], build[1], 1])
-                elif ([build[0], build[1] - 1, 0] in status) and ([build[0] + 1, build[1] - 1, 0] in status):
-                    status.remove([build[0], build[1], 1])
+                if ([x, y - 1, 0] not in status) and ([x + 1, y - 1, 0] not in status):
+                    if ([x - 1, y - 1, 0] in status) and ([x + 2, y - 1, 0] in status):
+                        status.remove([x, y, 1])
+                elif ([x, y - 1, 0] in status) and ([x + 1, y - 1, 0] in status):
+                    status.remove([x, y, 1])
                 else:
-                    if [build[0], build[1] - 1, 0] in status:
-                        if [build[0] + 1, build[1], 1] in status:
-                            if [build[0] + 2, build[1] - 1, 0] in status:
-                                status.remove([build[0], build[1], 1])
+                    if [x, y - 1, 0] in status:
+                        if [x + 1, y, 1] in status:
+                            if [x + 2, y - 1, 0] in status:
+                                status.remove([x, y, 1])
                             continue
-                        if [build[0] + 1, build[1], 0] not in status:
-                            status.remove([build[0], build[1], 1])
+                        if [x + 1, y, 0] not in status:
+                            status.remove([x, y, 1])
                     else:
-                        if [build[0] - 1, build[1], 1] in status:
-                            if [build[0] - 1, build[1] - 1, 0] in status:
-                                status.remove([build[0], build[1], 1])
+                        if [x - 1, y, 1] in status:
+                            if [x - 1, y - 1, 0] in status:
+                                status.remove([x, y, 1])
                             continue
-                        if [build[0], build[1], 0] not in status:
-                            status.remove([build[0], build[1], 1])
+                        if [x, y, 0] not in status:
+                            status.remove([x, y, 1])
                         
     status.sort(key=lambda x:(x[0], x[1], x[2]))
     return status
+
+
+#
+####################################################################################################
---
+++ 유형별_기출/구현/07.py
@@ -0,0 +1,45 @@
+import itertools
+import copy
+
+# 치킨집 위치와 거리를 구하는 함수
+def chicken(town, y, x, store):
+  # 집과의 거리
+  dis = 1
+
+  while True:
+    for s in store:
+      if abs(s[0] - y) + abs(s[1] - x) == dis:
+        return dis
+    dis += 1
+
+n, m = map(int, input().split())
+# 마을 상태
+town = []
+# 치킨집 위치
+store = []
+for _ in range(n):
+  town.append(list(map(int, input().split())))
+
+# 치킨집 위치부터 저장
+for y in range(n):
+  for x in range(n):
+    if town[y][x] == 2:
+      store.append([y, x])
+
+# 남겨야하는 치킨집 개수만큼 조합생성
+lefted = list(itertools.combinations(store, m))
+
+chicken_distance = []
+
+# 치킨집이 남겨지는 상황 순회
+for situation in lefted:
+  # 상황마다 치킨거리 구하기
+  cd = 0
+  for y in range(n):
+    for x in range(n):
+      if town[y][x] == 1:
+        cd += chicken(town, y, x, situation)
+  chicken_distance.append(cd)
+
+# 도시의 치킨 거리의 최소값 출력
+print(min(chicken_distance))
